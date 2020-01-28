ada78cea3c6567c1cee536e5ba1db406f1115bec
Timo Frank <timo.frank@student.uni-luebeck.de>
2020-01-29 00:06:29 +0200

    test stuff

---
+++ Assets/Scripts/Buildings/BuildingSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Rendering;
+
+public class BuildingSpawner : MonoBehaviour
+{
+    [SerializeField] private Mesh mesh;
+    [SerializeField] private Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EntityManager entityManager = World.Active.EntityManager;   
+        
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(
+            typeof(HealthComponent),
+            typeof(Translation),
+            typeof(MeshRenderer),
+            typeof(LocalToWorld)            
+        );
+
+        Entity entity = entityManager.CreateEntity(entityArchetype);
+        entityManager.SetComponentData(entity, new HealthComponent { health = 100 });
+        entityManager.SetSharedComponentData(entity, new RenderMesh {
+            mesh = mesh,
+            material = material
+        });
+    }
+}
