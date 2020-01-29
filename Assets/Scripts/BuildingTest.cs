d59f109598427c164eaf5a28a4205f347a4c586e
Timo Frank <timo.frank@student.uni-luebeck.de>
2020-01-29 18:35:56 +0200

    made script to convert tilemapobjects to entities

--- Assets/Scripts/BuildingTest.cs
+++ Assets/Scripts/BuildingTest.cs
@@ -4,29 +4,34 @@ using UnityEngine;
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Rendering;
+using Unity.Mathematics;
 
-public class BuildingSpawner : MonoBehaviour
+public class BuildingTest : MonoBehaviour
 {
+    [SerializeField] private GameObject tilemapLayer;
     [SerializeField] private Mesh mesh;
     [SerializeField] private Material material;
 
     // Start is called before the first frame update
     void Start()
     {
-        EntityManager entityManager = World.Active.EntityManager;   
         
+        /*
+        EntityManager entityManager = World.Active.EntityManager;   
         EntityArchetype entityArchetype = entityManager.CreateArchetype(
-            typeof(HealthComponent),
             typeof(Translation),
-            typeof(MeshRenderer),
+            typeof(RenderMesh),
             typeof(LocalToWorld)            
         );
 
-        Entity entity = entityManager.CreateEntity(entityArchetype);
-        entityManager.SetComponentData(entity, new HealthComponent { health = 100 });
+        Entity entity = entityManager.CreateEntity(entityArchetype);        
         entityManager.SetSharedComponentData(entity, new RenderMesh {
             mesh = mesh,
-            material = material
+            material = material,
         });
+        entityManager.SetComponentData(entity, new Translation {
+            Value = new float3(0, 0, -1)
+        });
+        */
     }
 }
---
+++ Assets/Scripts/TilemapObjectsToEntities.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Mathematics;
+
+public class TilemapObjectsToEntities : MonoBehaviour
+{
+    [SerializeField] private Material material;
+    [SerializeField] private Mesh mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EntityManager entityManager = World.Active.EntityManager;   
+        EntityArchetype simpleEntityArchetype = entityManager.CreateArchetype(
+            typeof(Translation),
+            typeof(RenderMesh),
+            typeof(LocalToWorld)
+        );
+
+        Tilemap tilemap = GetComponent<Tilemap>();
+        foreach (var pos in tilemap.cellBounds.allPositionsWithin)
+        {   
+            Vector3Int localPlace = new Vector3Int(pos.x, pos.y, pos.z);
+            Vector3 place = tilemap.CellToWorld(localPlace);
+            if (tilemap.HasTile(localPlace))
+            {
+                TileBase tile = tilemap.GetTile(localPlace);
+                Entity entity = entityManager.CreateEntity(simpleEntityArchetype);
+                entityManager.SetSharedComponentData(entity, new RenderMesh {
+                    material = material,
+                    mesh = mesh
+                });
+                entityManager.SetComponentData(entity, new Translation {
+                    Value = new float3(place.x + 0.5f, place.y + 0.5f, -1)
+                });
+            }
+        }
+        tilemap.ClearAllTiles();
+    }
+}
