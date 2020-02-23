facd7c9a10bbab76794541cf3b3a05e9b2b94e68
desbanst <desbans.thomas@hotmail.com>
2020-02-23 18:47:02 +0200

    merge

--- Assets/Scripts/Systems/SaveSystem.cs
+++ Assets/Scripts/Systems/SaveSystem.cs
@@ -11,23 +11,19 @@ using UnityEngine.SceneManagement;
 
 public class SaveSystem : ComponentSystem
 {
-    public static List<Entity> listOfUnit = new List<Entity>();
     public static List<Entity> listOfHarvestable = new List<Entity>();
+    public int woodAmount = new int();
+    public int goldAmount = new int();
 
     protected override void OnUpdate()
-    {
-        /*if (SceneManager.GetActiveScene().name == "test")
+    {/*
+
+        woodAmount = UI.instance.getWoodAmount();
+        goldAmount = UI.instance.getGoldAmount();
+
+        for (int k=0; k < listOfHarvestable.Count ;  k++)
         {
-            if (SettingScript.isSaved)
-            {
-                Entities.WithAll<HarvestableComponent>().ForEach((Entity entity) =>
-                {
-                    if (!listOfHarvestable.Contains(entity))
-                    {
-                        DestroyHarvestable(entity);
-                    }
-                });
-            }
+
         }*/
     }
 
